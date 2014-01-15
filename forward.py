28bbef70b53d7d9137ea042339632ca9d74697e6
Patrick Malsom <patrickmalsom@gmail.com>
2014-01-15 12:13:48 -0500

    forward integration code that approximately solves for the mean transition time and takes a bunch of averages. This method has problems that are the motivation for persuing the KL-distance method.

--- forward.py
+++ forward.py
@@ -6,9 +6,10 @@ import math
 from time import clock
 from scipy import integrate
 import matplotlib.pyplot as plt
+import numpy as np
 
 # Define constants
-Nb= 100001 # maximum integration steps
+Nb= 200001 # maximum integration steps
 Nu=Nb-1 #total time steps
 eps=.25 # configurational temperature
 deltat = .001 #step size
@@ -24,10 +25,11 @@ def force(x):
   return -4.0*x*(x**2.0 -1.)
 
 savedPaths=0 # number of accepted sample paths
-totalPaths=100
-transOffset=2000 #integration steps to overshoot
-avePath=[0.0 for i in range(transOffset*2)] # intialize an array to store the path
-transMark=0.5 # position to count the transition at
+totalPaths=5000
+transOffset=4000 #integration steps to overshoot
+avePath=[0.0 for i in range(transOffset*2)] # intialize an array to store the ave path
+ave2Path=[0.0 for i in range(transOffset*2)] # intialize an array to store theavg squared path 
+transMark=1. # position to count the transition at
 
 while savedPaths<totalPaths:
   # Forward integration initialization
@@ -55,7 +57,7 @@ while savedPaths<totalPaths:
   #find middle of trans trigger
   tempIndex = index
   posTrigger=tempIndex-transOffset
-  while path[tempIndex-1] > -0.5:
+  while path[tempIndex-1] > -transMark:
     tempIndex-=1
   negTrigger=tempIndex
   middle=int(0.5*(posTrigger + negTrigger))
@@ -63,16 +65,24 @@ while savedPaths<totalPaths:
   print "index: %i i| pos: %f | saved: %i | %s %s %s " % (index, path[index-1], savedPaths, path[Nu] is 0.0, path[index-1]>0.0, index > transOffset*2)
   
   # decide to keep the path or not
-  if path[Nu] is 0.0 and path[index-1]>0.0 and index > transOffset*2:
+  if path[Nu] is 0.0 and path[index-1]>0.0 and index > transOffset*4 and index < Nb-(transOffset*4):
     # accepted path
     accPath=path[middle-transOffset:middle+transOffset]
     savedPaths+=1 #incriment the saved paths counter
-    tempPath=avePath
-    avePath=[accPath[i]+tempPath[i] for i in range(transOffset*2)]
-  
 
-tempPath=avePath
-avePath=[tempPath[i]/float(totalPaths) for i in range(transOffset*2)]
+    avePath=[accPath[i]+avePath[i] for i in range(transOffset*2)]
+
+    ave2Path=[accPath[i]**2.0+ave2Path[i] for i in range(transOffset*2)]
+
+avePath=[avePath[i]/float(totalPaths) for i in range(transOffset*2)]
+
+ave2Path=[ave2Path[i]/float(totalPaths) for i in range(transOffset*2)]
+
+np.savetxt("savePath.txt",np.array([[avePath[i],math.sqrt(ave2Path[i]-avePath[i]**2.0)] for i in range(transOffset*2)]))
 
 plt.plot(avePath)
+plt.plot([-avePath[-i-1] for i in range(transOffset*2)])
+plt.plot([5.0*math.sqrt(ave2Path[i]-avePath[i]**2.0) for i in range(transOffset*2)])
 plt.show()
+
+
