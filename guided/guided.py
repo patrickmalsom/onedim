a0cc2287e06a91c402c3d26c2621c5b790429a55
Patrick Malsom <patrickmalsom@gmail.com>
2014-02-14 16:15:19 -0500

    rearranged python program with better names for the variables. should be exactly the same as the previous commit but with better names. also includes the full plotting routines

--- guided/guided.py
+++ guided/guided.py
@@ -1,21 +1,26 @@
 #!/bin/env python
 
-# ====================================
-# import libraries
+# ==========================================================================
+# Import Libraries
+# ==========================================================================
 import time
 import math
 import numpy as np
 import matplotlib.pyplot as plt
-from subprocess import call
-
 import ctypes
 
-GenPathC=ctypes.CDLL("/home/patrick/git/onedim/guided/GenPaths.so")
-CGenPaths=GenPathC.GenPaths
-
-
-# ====================================
-RunName="testa"
+# ==========================================================================
+# Constants of the simulation
+# ==========================================================================
+Nb=10001
+Nu=Nb-1
+NumPaths=1001
+eps=0.15
+xStart=0.969624
+deltat=0.001
+h=math.sqrt(2.0*deltat)
+T=Nu*deltat
+pref=math.sqrt(2.0*eps*deltat)
 
 #m0
 mPlus=0.969624
@@ -28,44 +33,23 @@ AHalf=-7.0
 gammaA=10.0
 tA=5.0
 
-# define some constants of the simulation
-Nb=10001
-Nu=Nb-1
-NumPaths=10001
-eps=0.15
-xStart=0.969624
-deltat=0.001
-h=math.sqrt(2.0*deltat)
-T=Nu*deltat
-pref=math.sqrt(2.0*eps*deltat)
+# ==========================================================================
+# C struct setup
+# ==========================================================================
+# import the c code library to generate paths
+GenPathsLibrary=ctypes.CDLL("GenPaths.so")
+# name the path generation function for convienence
+CGenPaths=GenPathsLibrary.GenPaths
 
-# ====================================
-def mZero(t):
-  return mPlus*math.tanh(gammam * (t-tm))
-
-def AZero(t):
-  return APlus + AHalf*math.exp(-gammaA*(t-tA)**2)
-
-#def GenPaths(numPaths,mlist,Alist,RNGSeed):
-#  mSaveName="mlist.dat"
-#  ASaveName="Alist.dat"
-#  np.savetxt(mSaveName,mlist)
-#  np.savetxt(ASaveName,Alist)
-#  call(["./guidedGenPaths.out",str(numPaths),str(RNGSeed),RunName,mSaveName,ASaveName])
-#  return np.loadtxt("outfiles/"+RunName+"-"+str(numPaths)+".dat")
-# ====================================
-# make the control function lists
-mlist=[ mZero(t) for t in np.linspace(0,10,Nb)]
-Alist=[ AZero(t) for t in np.linspace(0,10,Nb)]
-
-# ====================================
-
+# define some c variable types
 DOUBLE=ctypes.c_double
 PDOUBLE=ctypes.POINTER(DOUBLE)
 INT=ctypes.c_int
+PINT=ctypes.POINTER(INT)
 
-
-class Cbead(ctypes.Structure):
+# make a class analogous to the struct in GenPaths.c
+# this struct will be passed back with averages computed
+class Averages(ctypes.Structure):
   _fields_ = [('m', DOUBLE),
               ('A', DOUBLE),
               ('xbar', DOUBLE),
@@ -73,58 +57,83 @@ class Cbead(ctypes.Structure):
               ('expVal', DOUBLE*5)
              ]
 
-bead=Cbead*Nb
-
-CGenPaths.argtypes = [INT,bead]
+# define the array of averages that is to be passed to the C routine
+beadType=Averages*Nb
+# declare what arguments the function takes
+CGenPaths.argtypes = [INT,beadType]
 CGenPaths.restype = None
 
-bead_Array = bead()
+# ==========================================================================
+# Function declarations
+# ==========================================================================
+def mZero(t):
+# base function for the mean: tanh
+  return mPlus*math.tanh(gammam * (t-tm))
 
-for i in range(Nb):
-  bead_Array[i].m=mlist[i]
-  bead_Array[i].A=Alist[i]
-  bead_Array[i].xbar=0.0
-  bead_Array[i].xxbar=0.0
-  for j in range(5):
-    bead_Array[i].expVal[j]=0.0
+def AZero(t):
+# base function for the width: gaussian well
+  return APlus + AHalf*math.exp(-gammaA*(t-tA)**2)
 
+def GenPaths(loops, mlist, Alist):
+# call to the C library that will generate a path
+# inputs:
+#   loops: number of trajectories to calculate
+#   mlist: input list for the controlling mean
+#   Alist: input list for the controlling width
+# outputs: 
+#   returns an array with the expectation values (see wiki)
+  # allocate a new bead instance
+  bead = beadType()
+  # initialize the struct to pass to the c routine
+  for i in range(Nb):
+    bead[i].m=mlist[i]
+    bead[i].A=Alist[i]
+    bead[i].xbar=0.0
+    bead[i].xxbar=0.0
+    for j in range(5):
+      bead[i].expVal[j]=0.0
+  # call the C routine
+  CGenPaths(ctypes.c_int(loops),bead)
+  # return a numpy array with the averages computed
+  return np.array([ [bead[i].m,
+                     bead[i].A,
+                     bead[i].xbar,
+                     bead[i].xxbar,
+                     bead[i].expVal[0],
+                     bead[i].expVal[1],
+                     bead[i].expVal[2],
+                     bead[i].expVal[3],
+                     bead[i].expVal[4] 
+                    ] for i in range(Nb) ])
 
- 
-#doubleList= ctypes.c_double * Nb
-#mlistC=doubleList(mlist)
-#AlistC=doubleList(Alist)
+# ==========================================================================
+# Main routine
+# ==========================================================================
+# make the control function lists
+mlist=[ mZero(t) for t in np.linspace(0,10,Nb)]
+Alist=[ AZero(t) for t in np.linspace(0,10,Nb)]
 
-CGenPaths(ctypes.c_int(10000),bead_Array)
+#generate the paths and store stats in klstats array
+klstats=GenPaths(NumPaths,mlist,Alist)
 
-plt.plot([bead_Array[i].xbar for i in range(Nb)])
+# ==========================================================================
+# Plots!
+# ==========================================================================
+timePlt=[t for t in np.linspace(0,T,Nb)]
+f, axarr = plt.subplots(2,2)
+
+# A plot
+axarr[0,0].plot(timePlt, Alist)
+axarr[0,0].axis([0, 10, 0, 10])
+
+# m plot
+axarr[0,1].plot(timePlt, mlist)
+axarr[0,1].plot(timePlt, 1.0/NumPaths*(klstats[:,2]))
+
+# dD/dA plot
+axarr[1,0].plot(timePlt, 1.0/NumPaths*((klstats[:,4]*klstats[:,7]) - klstats[:,8]) )
+
+# dD/dm plot
+axarr[1,1].plot(timePlt, 1.0/NumPaths*((klstats[:,4]*klstats[:,5]) - klstats[:,6]) )
+
 plt.show()
-
-
-
-#mlistC=np.array(mlist,dtype=ctypes.c_double)
-#AlistC=np.array(Alist,dtype=ctypes.c_double)
-
-#klstats=GenPaths(NumPaths,mlist,Alist,10)
-
-# ====================================
-# PLOTS
-# ====================================
-
-#timePlt=[t for t in np.linspace(0,T,Nb)]
-#f, axarr = plt.subplots(2,2)
-#
-## A plot
-#axarr[0,0].plot(timePlt, Alist)
-#axarr[0,0].axis([0, 10, 0, 10])
-#
-## m plot
-#axarr[0,1].plot(timePlt, mlist)
-#axarr[0,1].plot(timePlt, 1.0/NumPaths*(klstats[:,2]))
-#
-## dD/dA plot
-#axarr[1,0].plot(timePlt, 1.0/NumPaths*((klstats[:,4]*klstats[:,7]) - klstats[:,8]) )
-#
-## dD/dm plot
-#axarr[1,1].plot(timePlt, 1.0/NumPaths*((klstats[:,4]*klstats[:,5]) - klstats[:,6]) )
-#
-#plt.show()
