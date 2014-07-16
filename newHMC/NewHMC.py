f40533013cec9403a52aa5e5cd3960689757f48e
Patrick Malsom <patrickmalsom@gmail.com>
2014-07-16 01:01:33 -0400

    pushed the entire MALA code into 2 function calls. still need to think the data structure through

--- newHMC/NewHMC.py
+++ newHMC/NewHMC.py
@@ -2,19 +2,26 @@
 
 import numpy as np
 import math
-from pylab import *
 
+# =================================================================
 # Constants
 deltat=0.001
 invdt=1/deltat
 eps=0.15
 deltatau=0.00001
 noisePref=math.sqrt(4.0*eps*deltatau*invdt)
+r=deltatau*0.5*invdt*invdt
 
+# =================================================================
 # ## Read in Path
-inPath=loadtxt("SDEPath-fat-skinny.dat")
+inPath=np.loadtxt("SDEPath-fat-skinny.dat")
 Num=len(inPath)
 
+# =================================================================
+# seed the RNG
+np.random.seed(100)
+
+# =================================================================
 # Function declarations
 def Pot(x):
     return 1.+ x*x*(-3.375+x * (1.6875 +x * (2.84766 +(-2.84766+0.711914 * x) * x)))
@@ -45,15 +52,21 @@ def quadVar(poslist):
     print "quad var: " + str(sum([(poslist[i]-poslist[i+1])**2 for i in range(len(poslist)-1)])/(2.*eps*deltat*(len(poslist)-1)))
 
 
+def GaussElim(bVec):
 # Solves Mx=b where
 #  M is tridiagonal matrix
 #    mainDiag: Length Num-2 (BC's)
 #    upper(lower)Diag: Length Num-3
 #  x is unknown and returned at the end
 #  b is known vector
-def GaussElim(mainDiag,lowerDiag,upperDiag,bVec):
+
     # size of the matrix
-    mainLen=len(mainDiag)
+    mainLen=len(bVec)
+
+    # make the main/lower/upper diagonal lists
+    mainDiag=[1+2*r for i in range(mainLen)]
+    lowerDiag=[-r for i in range(mainLen-1)]
+    upperDiag=[-r for i in range(mainLen-1)]
 
     # Gaussian Elimination down the lower diag
     for i in range(mainLen-1):
@@ -76,47 +89,42 @@ def GaussElim(mainDiag,lowerDiag,upperDiag,bVec):
     # return the result
     return bVec
 
+def genRHS(inputpath):
+    # (I+hL)x0
+    matxlist=0.0
+    # g_n
+    glist=0.0
+    # noise
+    noiselist=0.0
+    knownVec=[0.0 for i in np.arange(1,len(inPath)-1,1)]
 
-# ## generate x0 vector on RHS
-matxlist=[0.0 for i in arange(1,len(inPath)-1,1)]
-r=deltatau*0.5*invdt*invdt
-for i in arange(1,len(inPath)-1,1):
-    matxlist[i-1]=r*inPath[i-1] + (1-2.*r)*inPath[i] + r*inPath[i+1]
+    # calculate the nth element of the known vector on the RHS
+    for i in np.arange(1,len(inPath)-1,1):
+        matxlist=r*inPath[i-1] + (1-2.*r)*inPath[i] + r*inPath[i+1]
+        glist= deltatau * g(inPath[i-1],inPath[i],inPath[i+1])
+        noiselist= noisePref*np.random.normal(1,1)
+        knownVec[i-1]= matxlist-glist+noiselist
 
-# ## generate g list
-glist=[0.0 for i in arange(1,len(inPath)-1,1)]
-for i in arange(1,len(inPath)-1,1):
-    glist[i-1]= deltatau * g(inPath[i-1],inPath[i],inPath[i+1])
+    # adding the BC from the LHS matrix equation
+    knownVec[0]+=r*inPath[0]
+    knownVec[-1]+=r*inPath[-1]
 
+    return knownVec
 
-# ## generate noise list
-noiselist=[0.0 for i in arange(1,len(inPath)-1,1)]
-np.random.seed(100)
-for i in arange(1,len(inPath)-1,1):
-    noiselist[i-1]= noisePref*np.random.normal(1,1)
-
-
-# ## make full RHS vector
-rhs=[0.0 for i in arange(1,len(inPath)-1,1)]
-for i in arange(0,len(inPath)-2,1):
-    rhs[i]= matxlist[i]-glist[i]+noiselist[i]
-# adding the BC from the LHS matrix equation
-rhs[0]+=r*inPath[0]
-rhs[-1]+=r*inPath[-1]
+# =================================================================
+# generate the known RHS vector
+rhs=genRHS(inPath)
 
 # perform the gaussian elimination to find the new path x^1
-md=[1+2*r for i in range(len(inPath)-2)]
-ld=[-r for i in range(len(inPath)-3)]
-ud=[-r for i in range(len(inPath)-3)]
-xnewPath=GaussElim(md,ld,ud,rhs)
+xnewPath=GaussElim(rhs)
 
 # print the quadratic variation
 quadVar(xnewPath)
-
 # save the output path to file
-savetxt("NewHMC-out.dat",xnewPath)
+np.savetxt("NewHMC-out.dat",xnewPath)
 
 
+# =================================================================
 # ## Molecular Dynamics
 # 
 # Rather than solve for the next steps explicitly in terms of the previous two steps, as we did for the path space implementation, we are going to solve for the velocities and then use the same algorithm as the MALA above.
@@ -126,14 +134,11 @@ savetxt("NewHMC-out.dat",xnewPath)
 # 
 # After finding the velocities for the next step we can return to the top to perform the same *MALA* without choosing random velocities, thus producing the MD step.
 # 
-# 
-
 # ## Calculation of $L x$
 # 
 # The matrix L will need to be multiplied by a vector many times in this algorithm.
 # I will therefore make a function to do so.
 # 
-
 # ## Fourier Transform
 # 
 # * probably should use $2^n +1$ points, allows easy use of FFT
