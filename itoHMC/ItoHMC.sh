87c357fc6b9af38249d7674f6d651cdd7a736dcc
Patrick Malsom <patrickmalsom@gmail.com>
2014-12-24 14:40:57 -0500

    made the sh file as the wrapper for the python script. also added the necessary modules to load for running on the grid

--- itoHMC/ItoHMC.sh
+++ itoHMC/ItoHMC.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 
+########################################################################
+## Run on only on Open Science Grid
+## Import gcc via cvmfs
+#source /cvmfs/oasis.opensciencegrid.org/osg/modules/lmod/5.6.2/init/bash;
+#module load gcc/4.6.2;
+## Import python and numpy via cvmfs
+#source /cvmfs/oasis.opensciencegrid.org/osg/modules/lmod/5.6.2/init/bash;
+#module load python/2.7;
+#module load atlas;
+#module load lapack;
+#module load all-pkgs;
+########################################################################
+
+# Export the current dir as a library path 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$(pwd)
+
+# run the python script and pass all of the options to argparse
+./ItoHMC.py $@
