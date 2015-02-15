115ce962bef94ce8ab84f983825d4102c3b03f0b
Patrick Malsom <patrickmalsom@gmail.com>
2015-02-15 11:05:22 -0500

    hacked together a test function to make sure I am not doing things i shouldnt. Used for merge of ito and newHMC code bases

---
+++ newHMC/testPath.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+make clean 
+make
+
+shopt -s expand_aliases
+source ~/.bashrc
+
+./NewMALA.py -i fatterSkinny-T0p25-dt0p005-Nb30k-end-healed.dat -T 0.25 --deltat 0.005 --deltatau 0.000001 --Num 30001 --HMC 2 --MD 100 --RNGseed 10
+
+awk '{getline t<"outFileTesting.dat"; print $0-t}' outFile.dat > outFileDiff.dat
+cat outFileDiff.dat | termplot
