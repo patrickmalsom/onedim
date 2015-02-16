ece584b386c0316eb5c46cc4cfa6a9ee5071e6ea
Patrick Malsom <patrickmalsom@gmail.com>
2015-02-16 17:31:44 -0500

    moved the files around to be onedim instead of Ito. ready to merge the ito side

--- itoHMC/onedimHMC.py
+++ itoHMC/onedimHMC.py
@@ -107,7 +107,7 @@ INT=ctypes.c_int
 PINT=ctypes.POINTER(INT)
 
 # import the c code library
-clib=ctypes.CDLL("ItoHMC.so")
+clib=ctypes.CDLL("onedimHMC.so")
 
 # fill average position 
 c_calcPosBar=clib.calcPosBar
