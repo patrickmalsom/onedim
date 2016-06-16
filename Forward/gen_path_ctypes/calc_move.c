8c1a3641711855fc384faaca5484d8c3b70f9dba
Patrick Malsom <patrickmalsom@gmail.com>
2016-06-16 00:33:36 -0400

    started the transition to using C code only with a bash script to run the whole mess. command line args and gsl random numbers are working in this commit, but the sampling is not.

--- Forward/gen_path_ctypes/calc_move.c
+++ Forward/gen_path_ctypes/calc_move.c
@@ -2,10 +2,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// NOTE: The structs MUST EXACTLY MATCH the class in the python routine
-// Stores many useful constants. initialized in the python code
-typedef struct _parameters
-{
+//GNU Scientific Libraries
+#include <gsl/gsl_rng.h>
+#include <gsl/gsl_randist.h>
+#include <gsl/gsl_math.h>
+
+typedef struct _parameters{
   double dt;
   double eps;
   double noisePref;
@@ -15,55 +17,42 @@ typedef struct _parameters
   int MHMC; //boolean for MHMC on:1 off:0
 } parameters;
 
-// Array struct for the trajectory
-typedef struct _traj_array
-{
-  double grn;
-  double pos;
-  double rand;
-} traj_array;
 
-/* =====================================================================
- * Potential Definitions: Fatter-Skinnier Potential: (8 - 5*x)**8 * (2 + 5*x)**2 / 2**26
- * -----------------------------
- *   Pot:       U(x)          -> returns the potential at a position
- *   Force:     F(x)=-dU/dx   -> returns the force at a position
-*/
-
-// Calculate the fatter-skinny potential ( U(x) )
 double Pot(double x){
-  // Calculate: ((8 - 5 x)^8 (2. + 5 x)^2)/2^26
-  // Horner form for the potential:
+  // Calculate the fatter-skinny potential ( U(x) )
+  // Horner form of: ((8 - 5 x)^8 (2. + 5 x)^2)/2^26
   return x*x*(x*(x*(x*(x*(x*(x*(x*(0.145519152283669*x - 1.74622982740402) + 8.96397978067398) - 25.331974029541) + 41.7232513427734) - 37.384033203125) + 10.68115234375) + 9.765625) - 7.8125) + 1.0; 
-
-
 }
 
-//Calculate the fatter skinny Force ( F = - dU(x)/dx )
-double Force(double x){
 
-  // Calculate: -250*x*(5*x-8)**7*(5*x+2)/2**26
-  // Horner Form for the force
+double Force(double x){
+  // Calculate the fatter skinny Force ( F = - dU(x)/dx )
+  // Horner form of: -250*x*(5*x-8)**7*(5*x+2)/2**26
   return x*(x*(x*(x*(x*(x*(x*(x*(-1.45519152283669*x + 15.7160684466362) - 71.7118382453918) + 177.323818206787) - 250.339508056641) + 186.920166015625) - 42.724609375) - 29.296875) + 15.625);
 }
-// return the force at the mid point
+
+
 double midpt_Force(double x1,double x0){
+  // return the force at the mid point
   // F_w(x1,x0) = F( (x1+x0)/2 )
   return Force(0.5*(x1+x0));
 }
 
-// return the force according to simpsons method
+
 double simpson_Force(double x1,double x0){
+  // return the force according to simpsons method
   // F_w(x1,x0) = ( F(x1) + 4*F((x1+x0)/2) + F(x0) )/6
   return (Force(x1) + 4.0*Force(0.5*(x1+x0)) + Force(x0))*0.16666666666666666666;
 }
 
+
 double gen_leapfrog(double x0,double random_gauss, parameters params){
-  // return new step 
+  // return new step with leap frog quadrature
   return x0 + params.dt * Force(x0) + params.noisePref * random_gauss;
 }
 
 double gen_midpt(double x0,double random_gauss, parameters params){
+  // return new step with midpoint quadrature (implicit method)
   // save the initial point
   double xsave = x0;
   // guess the new step according to leap frog
@@ -78,6 +67,7 @@ double gen_midpt(double x0,double random_gauss, parameters params){
 }
 
 double gen_simpson(double x0,double random_gauss, parameters params){
+  // return new step with simpsons quadrature (implicit method)
   // save the initial point
   double xsave = x0;
   // guess the new step according to leap frog
@@ -91,6 +81,7 @@ double gen_simpson(double x0,double random_gauss, parameters params){
   return xguess;
 }
 
+/*
 int create_trajectory(traj_array* traj, parameters params){
   // Create the whole trajectory and keep track of B(s)
   int i;
@@ -148,4 +139,57 @@ int create_trajectory(traj_array* traj, parameters params){
   }
   return(MHMC_acc);
 }
+*/
 
+
+
+
+
+
+
+
+
+int main(int argc, char *argv[])
+{
+
+  //Declare params
+  parameters params;
+  //Initialize the params struct
+  // argument order: dt eps noisePref xstart num method MHMC
+  params.dt = atof(argv[1]);
+  params.eps = atof(argv[2]);
+  params.xstart = atof(argv[3]);
+  params.num = atoi(argv[4]);
+  params.method = atoi(argv[5]);
+  params.MHMC = atoi(argv[6]);
+
+  printf("dt %f\n", params.dt);
+  printf("eps %f\n",params.eps);
+  printf("xstart %f\n",params.xstart);
+  printf("num %i\n",params.num);
+  printf("method %i\n",params.method);
+  printf("MHMC %i\n",params.MHMC);
+
+  params.noisePref = sqrt(2.0*params.eps*params.dt);
+
+  //===============================================================
+  // GNU Scientific Library Random Number Setup
+  //===============================================================
+  // Example shell command$ GSL_RNG_SEED=123 ./a.out
+  printf("=======================================================\n");
+  const gsl_rng_type * RanNumType;
+  gsl_rng *RanNumPointer; 
+  gsl_rng_env_setup();
+  RanNumType = gsl_rng_default;
+  RanNumPointer= gsl_rng_alloc (RanNumType);
+  printf("Random Number Generator Type: %s \n", gsl_rng_name(RanNumPointer));
+  printf("RNG Seed: %li \n", gsl_rng_default_seed);
+  printf("=======================================================\n");
+
+
+  printf("%f\n",gsl_rng_uniform(RanNumPointer));
+  printf("%f\n",gsl_ran_gaussian(RanNumPointer,1));
+
+  gsl_rng_free (RanNumPointer);
+
+}
